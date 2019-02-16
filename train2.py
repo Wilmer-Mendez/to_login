502703de8cc3f5b5d1ac8da69f903fe67ec816d5
Wilmer-Mendez <47387782+Wilmer-Mendez@users.noreply.github.com>
2019-02-16 22:49:40 +0800

    Update train2.py

--- train2.py
+++ train2.py
@@ -40,35 +40,14 @@ cos = ibm_boto3.resource('s3',
 ################################################################################
 # Choose Bucket
 ################################################################################
-'''def askForBucket():
-  bucket_list = []
-  for i, bucket in enumerate(cos.buckets.all()):'''
+
   bucket_list.append(bucket.name)
   print('  {}) {}'.format(i + 1, bucket.name))
 
-  bucket_id_name = '1'
+  bucket_id_name = 1
 
   if bucket_id_name in bucket_list:
     return bucket_id_name
- ''' else:
-    try:
-      bucket_id_name = int(bucket_id_name)
-      if bucket_id_name <= len(bucket_list) and bucket_id_name > 0:
-        return bucket_list[bucket_id_name - 1]
-      else:
-        print('\nPlease choose a valid bucket:')
-        return askForBucket()
-    except ValueError:
-      print('\nPlease choose a valid bucket:')
-      return askForBucket()
-
-if args.data_bucket_name == None:
-  print('\nPlease choose a training \033[92mdata\033[0m bucket:')
-  args.data_bucket_name = askForBucket()
-
-if args.result_bucket_name == None:
-  print('\nPlease choose a training \033[92mresults\033[0m bucket:')
-  args.result_bucket_name = askForBucket()'''
 
 
 ################################################################################
